939586a04527fb8ea4653b10f1792c6a028dccb1
Ederlin Ortega <ederling91@gmail.com>
2026-05-28 20:58:04 -0400

    Actualizacion

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.use(morgan('dev'))
+
+app.get('/', (req, res) =>{
+    res.render('index', { message: 'Hello From Node.js' })
+})
+
+app.get('/contact', (req, res) => {
+    res.render('index', { message: 'The Contact Page' })
+})
+
+app.get('/about', (req, res) => {
+    res.render('index', { message: 'The About Page' })
+})
+
+//Ruta comodin
+app.use((req, res) => {
+    res.status(404).render('index', { message: 'Not Found' })
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
